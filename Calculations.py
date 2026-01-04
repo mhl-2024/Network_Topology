2625db3fc2998debd3a000f7ebdc95bada3189e9
MinhuiLi <minhui0603@gmail.com>
2026-01-03 22:29:33 -0500

    Refactor Calculations.py to remove redundant code

--- Calculations.py
+++ Calculations.py
@@ -145,26 +145,3 @@ for id0 in outletID[start:end]:
 # ===== S5. save the files ============
 # run on HPC cluster
 Tk_df.to_csv('Tk_df_v'+str(start)+'.csv',index=False)
-
-# %%   junction angle aggregation to get basin-averaged values
-junfolder = ''
-jundf = pd.read_csv(os.path.join(junfolder,'JunctionAngles.csv'))
-
- # sidebranching angle
-sidejun = jundf[jundf['deltaHS']>0]
- # bifurcation angle
-bijun = jundf[jundf['deltaHS']==0] 
-
-sideangle = sidejun.groupby('outletNHDid').agg(
-    Sidebranch=('angle','mean')).reset_index()
-biangle = bijun.groupby('outletNHDid').agg(
-    Bifurcation=('angle','mean')).reset_index()
-branchangle = jundf.groupby('outletNHDid').agg(
-    Branching=('angle','mean')).reset_index()
-
-Network_angles=sideangle.merge(biangle,on='outletNHDid',how='inner')
-Network_angles=Network_angles.merge(branchangle,on='outletNHDid',how='inner')
-
-Network_angles.to_csv('JunctionAngles.csv',index=False)
-
-
